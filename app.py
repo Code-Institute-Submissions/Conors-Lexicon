cb69029f5047e844fb5bdfaa2d71b1e68ea333b8
conor52 <conormccarthy157@gmail.com>
2020-08-29 08:23:17 +0000

    tweaked app.py as per new video tutorial for deploying to heroku

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+if os.path.exists("env.py")
+    import env
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DB_NAME"] = "dictionary_manager"
+app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster.r2bey.mongodb.net/dictionary_manager?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+
+
+@app.route("/get_index")
+def get_dictionary():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
